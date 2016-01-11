f5e2fa1747da129bb24d1fe61803f7d4ff39152a
elise rozenberg <eliserozenberg@gmail.com>
2016-01-10 22:25:18 -0500

    update projectile

--- app/src/main/java/rozenberg/projectile/MainActivity.java
+++ app/src/main/java/rozenberg/projectile/MainActivity.java
@@ -1,6 +1,7 @@
 package rozenberg.projectile;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -17,9 +18,6 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
 
-    private TextView textAngle;
-    private TextView textVelocity;
-    private TextView textTime;
     private TextView textAns;
 
     private EditText editTextVelocity;
@@ -28,38 +26,38 @@ public class MainActivity extends AppCompatActivity {
 
     private Button buttonCalc;
 
-    private double angle,velocity,time;
-
-
+    private double angle, velocity, time;
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //name - mode - restrcition that they have with the rest of the application
+        preferences = this.getSharedPreferences("DEFAULT", MODE_PRIVATE);
         imageView = (ImageView) findViewById(R.id.image2);
         Picasso.with(this)
                 .load("http://jokideo.com/wp-content/uploads/2013/03/226431_518712648181745_1707739609_n.jpg")
                 .error(R.drawable.error_image)
                 .into(imageView);
 
-        textAngle= (TextView) findViewById(R.id.text1);
-        editTextAngle= (EditText) findViewById(R.id.editText1);
-        textVelocity= (TextView) findViewById(R.id.text2);
-        editTextVelocity= (EditText) findViewById(R.id.editText2);
-        textTime= (TextView) findViewById(R.id.text3);
-        editTextTime= (EditText) findViewById(R.id.editText3);
+
+        editTextAngle = (EditText) findViewById(R.id.editText1);
+        editTextVelocity = (EditText) findViewById(R.id.editText2);
+        editTextTime = (EditText) findViewById(R.id.editText3);
         buttonCalc = (Button) findViewById(R.id.button1);
-        textAns= (TextView) findViewById(R.id.textAns);
+        textAns = (TextView) findViewById(R.id.textAns);
         //assuming have the image thing paste the line of code
         //replace context - with this
 
         buttonCalc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                calculateProjectile();}
+                calculateProjectile();
+            }
         });
-}
+    }
 
     private void calculateProjectile() {
         angle = Double.parseDouble(editTextAngle.getText().toString());
@@ -68,13 +66,12 @@ public class MainActivity extends AppCompatActivity {
         //Projectile projectile = new Projectile(angle,velocity,time);
         //textAns.setText("x = " +  projectile.getX() + "  y= " + projectile.getY() );
 
-        Intent intent = new Intent(this,AnswerActivity.class);
+        Intent intent = new Intent(this, AnswerActivity.class);
         //send the data to intent
-        intent.putExtra("Angle" , angle);
-        intent.putExtra("Velocity" , velocity);
-        intent.putExtra("Time" , time);
+        intent.putExtra("Angle", angle);
+        intent.putExtra("Velocity", velocity);
+        intent.putExtra("Time", time);
         startActivity(intent);
-
     }
 
     @Override
@@ -95,7 +92,26 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = preferences.edit();
+        //like a hashmap - there is a key and a value
+        editor.putString("ANGLE", editTextAngle.getText().toString());
+        editor.putString("VELOCITY", editTextVelocity.getText().toString());
+        editor.putString("TIME", editTextTime.getText().toString());
+        editor.apply();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        editTextAngle.setText(preferences.getString("ANGLE", ""));
+        editTextVelocity.setText(preferences.getString("VELOCITY",""));
+        editTextTime.setText(preferences.getString("TIME",""));
+    }
+
 }
